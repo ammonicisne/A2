d2dd66040e4c81a9b17f739f049382f88d707b38
Jordan Hudgens <jordan@devcamp.com>
2017-06-15 16:52:54 +0000

    STOP LOOKING AT ME

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
   def home
     @posts = Blog.all
+<<<<<<< HEAD
     @skills = Skill.all
+=======
+>>>>>>> STOP LOOKING AT ME
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
 <<<<<<< HEAD
+<<<<<<< HEAD
   resources :portfolios
+=======
+>>>>>>> STOP LOOKING AT ME
   get 'pages/home'
 
   get 'pages/about'
 
   get 'pages/contact'
 
+<<<<<<< HEAD
 =======
 >>>>>>> Initial commit
+=======
+>>>>>>> STOP LOOKING AT ME
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
