77db66ab6ab0bb156a8d1bfc5889bf28fd32de43
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 15:06:24 +0000

    Update 1.5 -Created seeds file for sample datA

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,41 @@
 10.times do |blog|
   Blog.create!(
-      title: "My Blog Post #{blog}"
-      body: 'Body Content Body Content Body Content Body ContentBody ContentBody Content
-        Body Content Body Content Body ContentBody ContentBody ContentBody ContentBody Content Body Content 
-        Body Content Body Content Body Content Body ContentBody Content Body ContentBody Content Body Content 
-        Body ContentBody Content Body Content Body Content Body Content Body Content Body Content Body Content 
-        Body Content Body Content Body Content'
-    )
-end
-
-puts "10 blog posts created"
-
-5.times do |skill|
-  Skill.create!(
-      title: "rilas #{skill}",
-      percent_utilized: 15
+    title: "My Blog Post #{blog}",
+    body: "Body content Body content Body content Body content Body content Body content
+      Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content
+      Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content
+      Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content
+      Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content
+      Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content
+      Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content
+      Body content Body content Body content Body content Body content"
     )
   end
   
-  puts "5 Skills posts created"
+  puts "10 blog posts created"
   
-  9.times do [portfolio_items]
-    Portfolio.create!(
-        title: "Portfolio title: #{portfolio_items}",
-        subtitle: "My great service",
-        body: "Body Content Body Content Body Content Body ContentBody ContentBody Content
-          Body Content Body Content Body ContentBody ContentBody ContentBody ContentBody Content Body Content 
-          Body Content Body Content Body Content Body ContentBody Content Body ContentBody Content Body Content 
-          Body ContentBody Content",
-        main_image: http://placehold.it/360x360,
-        thumb_image: http://placehold.it/36x360,
-        
+  5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
       )
-  end
-  
-  puts "9 portfolio item created"
+    end
+    
+     puts "5 skills created"
+     
+     9.times do |portfolio_item|
+       Portfolio.create!(
+          title: "Portfolio title: #{portfolio_item}",
+          subtitle: "My great service",
+          body: "Body content Body content Body content Body content Body content Body content Body content Body content
+      Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content
+      Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content
+      Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content Body content
+      Body content Body content Body content Body content Body conten",
+          main_image: "http://placehold.it/600x400",
+          thumb_image: "http://placehold.it/350x150"
+         )
+     
+     end
+     
+      puts "9 portfolio items created"
