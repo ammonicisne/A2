d2aaa24520df5e8c3c265194540b291468d08409
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 16:26:37 +0000

    Added new parameters for user registration and editing account

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170615173518) do
 =======
 ActiveRecord::Schema.define(version: 20170614142947) do
@@ -51,6 +52,9 @@ ActiveRecord::Schema.define(version: 20170626165337) do
 =======
 ActiveRecord::Schema.define(version: 20170628154348) do
 >>>>>>> Implemented devise
+=======
+ActiveRecord::Schema.define(version: 20170628154348) do
+>>>>>>> Added new parameters for user registration and editing account
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,6 +133,24 @@ ActiveRecord::Schema.define(version: 20170628154348) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "name"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_user_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_user_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "users", force: :cascade do |t|
