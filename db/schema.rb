971b8c2c4ade189691993b11eec0d1a613cd2d8f
ammonIcisne <ammon.15.2015.ci@gmail.com>
2017-07-10 14:35:42 +0000

    Update 3.3

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 20170628154348) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "name"
@@ -90,5 +90,4 @@ ActiveRecord::Schema.define(version: 20170628154348) do
   end
 
   add_foreign_key "blogs", "topics"
-
 end
