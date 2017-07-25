1964c0c9b9edfa2ffcbd49a009da1db5ce325697
ammonIcisne <ammon.15.2015.ci@gmail.com>
2017-07-25 15:56:12 +0000

    Update 5.4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,5 +19,53 @@ module ApplicationHelper
   def copyright_generator
     AmmonViewTool::Renderer.copyright 'ammon Cisneros', 'All rights reserved'
   end
-end
+  
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      }
+    ]
+    [
+      {
+        url: about_me_path,
+        title: 'About Me'
+      }
+    ]
+    [
+      {
+        url: contact_me_path,
+        title: 'Contact'
+      }
+    ]
+    [
+      {
+        url: blogs_me_path,
+        title: 'Blog'
+      }
+    ]
+    [
+      {
+        url: portfolios_me_path,
+        title: 'Portfolios'
+      }
+    ]
+  end
+  
+  
+  def nav_helper style, tag_type
+    nav_links = ''
+    
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='{style} #{active? item[:url]}'>#{item[:url]}</a></#{tag_type}>"
+    end
 
+    nav_links.html_safe
+  end
+  
+  def active? path
+    "active" if current_page? path
+  end
+  
+end
