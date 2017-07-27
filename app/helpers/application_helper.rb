60e2eed8792e7db17ad58c3a60b2da9d6efaa429
ammonIcisne <ammon.15.2015.ci@gmail.com>
2017-07-27 15:25:49 +0000

    Update 6.2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -68,4 +68,23 @@ module ApplicationHelper
     "active" if current_page? path
   end
   
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    
+    if alert
+      alert_generator alert
+    end
+  end
+  
+  def alert_generator msg
+      js add_gritter(msg, title: "Ammon Cisneros Portfolio", sticky: false)
+  end
+  
+  
+  
+  
+  
+  
+  
+  
 end
