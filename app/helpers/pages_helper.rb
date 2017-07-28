4ed814f97e9fea6ad456a33f36584511a9349597
ammonIcisne <ammon.15.2015.ci@gmail.com>
2017-07-28 06:33:38 +0000

    Update 6.3

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,12 @@
 module PagesHelper
+  def twitter_parser tweet
+   #THIS IS INFORMATION THAT WAS NOT ADDED"
+   
+   #Building a Parser View Helper Method to Make Twitter Links Clickable in Rails
+    #Section 7
+    
+    tweet.gsub(regex) do |url|
+      "<a href=#{url} target='_blank'>#{url}</a>"
+    end.html_safe
+  end
 end
