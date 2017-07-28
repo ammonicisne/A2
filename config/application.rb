09f4bec61b315ea2f9565e3ba87b28c06eef9a64
ammonIcisne <ammon.15.2015.ci@gmail.com>
2017-07-28 05:43:24 +0000

    Update 6.3

--- config/application.rb
+++ config/application.rb
@@ -18,12 +18,7 @@ Bundler.require(*Rails.groups)
 
 module AnuheaLol
   class Application < Rails::Application
-     config.generators do |g|
-      g.orm             :active_record
-      g.template_engine :erb
-      g.test_framework  :test_unit, fixture: false
-      g.stylesheets     false
-      g.javascripts     true
+      config.eager_load_paths <<"#{Rails.root}/lib"
     end
   end
 end
